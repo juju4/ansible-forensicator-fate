55c2bdd8fc8e88e918085607a137ebb200c6b097
juju4 <juju4@users.noreply.github.com>
2019-03-02 18:39:39 -0500

    add extra role dependency: sleuthkit

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -12,6 +12,7 @@ fi
 [ ! -d $rolesdir/juju4.redhat_epel ] && git clone https://github.com/juju4/ansible-redhat-epel $rolesdir/juju4.redhat_epel
 [ ! -d $rolesdir/juju4.sift ] && git clone https://github.com/juju4/ansible-sift $rolesdir/juju4.sift
 [ ! -d $rolesdir/juju4.gift ] && git clone https://github.com/juju4/ansible-gift $rolesdir/juju4.gift
+[ ! -d $rolesdir/juju4.sleuthkit ] && git clone https://github.com/juju4/ansible-sleuthkit.git $rolesdir/juju4.sleuthkit
 [ ! -d $rolesdir/juju4.volatility ] && git clone https://github.com/juju4/ansible-volatility $rolesdir/juju4.volatility
 [ ! -d $rolesdir/juju4.rekall ] && git clone https://github.com/juju4/ansible-rekall $rolesdir/juju4.rekall
 [ ! -d $rolesdir/ernestas-poskus.docker ] && git clone https://github.com/ernestas-poskus/ansible-role-docker.git $rolesdir/ernestas-poskus.docker
